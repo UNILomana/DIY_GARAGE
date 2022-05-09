f255f88eb1124ac4fb9cc300bb6f79a32964e496
UNILomana <lomana.markel@uni.eus>
2022-05-09 14:13:12 +0200

    Comprobar bookings DB

--- WEB_PAGE_V3/Bookings_PHP/PHP_Bookings.php
+++ WEB_PAGE_V3/Bookings_PHP/PHP_Bookings.php
@@ -33,51 +33,55 @@ function insert_bookings()
   $hours = $_POST['use_hours'];
 
 
-  //COMPRUEBA QUE LAS FECHAS ESTAN DISPONIBLES
-  $kabina = mysqli_query($link, "select * from cabins where Type = '$type' and Disponibility = '1'");
-  $emaitza2 = mysqli_num_rows($kabina);
-
-  $lerroa = mysqli_fetch_assoc($kabina);
-  $cabin = $lerroa['Cabin_Id'];
-  $helper = $lerroa['Helper'];
+  //Elegir la cabina y el ayudante SEGUN TYPE OF VEHICLE
+  $kabina = mysqli_query($link, "select * from cabins where Type = '$type'");
+  $kabina_lerroa = mysqli_fetch_assoc($kabina);
+  $cabin = $kabina_lerroa['Cabin_Id'];
+  $helper = $kabina_lerroa['Helper'];
   
-  if ($emaitza2 == 0) {
+  //COMPRUEBA QUE LAS FECHAS ESTAN DISPONIBLES
+  $fecha= mysqli_query($link, "select * from bookings where Date = '$date' and Hour= '$time' and Cabin_Id ='$cabin'"); //comprueba si existe alguna reserva con esa fecha
+  $fecha_lerroa = mysqli_num_rows($fecha);
+
+  if ($fecha_lerroa > 0) {     
     header("Location: ./Bookings.php?incorrect=yes");
-
-    //$kabina = mysqli_query($link, "select * from cabins where Type = '$type' and Disponibility = '1'");
-  } else if ($emaitza2 > 0) {
-    $kaixo = mysqli_query($link, "select * from bookings where Date = '$date' and Hour= '$time' and Cabin_Id ='$cabin'"); //comprueba si existe alguna reserva con esa fecha
-    $ilara = mysqli_num_rows($kaixo);
-
-    if ($ilara > 0) {     
-      header("Location: ./Bookings.php?incorrect=yes");
-    } else {
-
-      
-      //TOTAL_PRICE CALCULO
-      $price = mysqli_query($link, "select * from vehicles where Type = '$type'");
-      $row = mysqli_fetch_assoc($price);
-      //Horas de trabajo a pagar en caso de necesitar ayuda
-      if ($help == 'yes') {
-        $worker_hours = $hours * 15;
-      } else if ($help == 'no') {
-        $worker_hours = 0;
-      }
+  } else {
+    //TOTAL_PRICE CALCULO
+    $price = mysqli_query($link, "select * from vehicles where Type = '$type'");
+    $price_lerroa = mysqli_fetch_assoc($price);
+    //Horas de trabajo a pagar en caso de necesitar ayuda
+    if ($help == 'yes') {
+      $worker_hours = $hours * 15;
+    } else if ($help == 'no') {
       $worker_hours = 0;
-      $total_price = ((float)$row['Price_Hour'] * (int)$hours + (float)$worker_hours);
+    }
+    $total_price = ((float)$price_lerroa['Price_Hour'] * (int)$hours + (float)$worker_hours);
 
 
-      //Bookings_Id al ser AUTO_INCREMENT hay que dejarlo vacío
-      //Cuidado con el NULL ha dado problemas
-      if ($help == 'yes') {
-        $result = mysqli_query($link, "insert into bookings values ('','$sesioa','$cabin','$date', '$time', '$type', '$help', '$helper' , '$hours', '$total_price')");
-      } else if ($help == 'no') {
-        $result = mysqli_query($link, "insert into bookings values ('','$sesioa','$cabin','$date', '$time', '$type', '$help', NULL, '$hours', '$total_price')");
-      }
+    /*EN CASO DE QUITAR LAS HORAS */
+    /*
+    //TOTAL_PRICE CALCULO
+    $price = mysqli_query($link, "select * from vehicles where Type = '$type'");
+    $price_lerroa = mysqli_fetch_assoc($price);
+    //Horas de trabajo a pagar en caso de necesitar ayuda
+    if ($help == 'yes') {
+      $worker_hours_price = 25;
+    } else if ($help == 'no') {
+      $worker_hours = 0;
+    }
+    $total_price = ((float)$price_lerroa['Price_Hour'] + (float)$worker_hours_price);*/
 
-      mysqli_close($link);
-      header("Location: ./Bookings.php?incorrect=no");
+
+    //Bookings_Id al ser AUTO_INCREMENT hay que dejarlo vacío
+    //Cuidado con el NULL ha dado problemas
+    if ($help == 'yes') {
+      $result = mysqli_query($link, "insert into bookings values ('','$sesioa','$cabin','$date', '$time', '$type', '$help', '$helper' , '$hours', '$total_price')");
+    } else if ($help == 'no') {
+      $result = mysqli_query($link, "insert into bookings values ('','$sesioa','$cabin','$date', '$time', '$type', '$help', NULL, '$hours', '$total_price')");
+    }
+
+    mysqli_close($link);
+    header("Location: ./Bookings.php?incorrect=no");
     }
   }
-}
 ?>
