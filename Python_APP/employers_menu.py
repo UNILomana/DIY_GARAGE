15cf9dbfd27c1e41719b2e701f130f3dcd8b66de
UNILomana <lomana.markel@uni.eus>
2022-05-16 09:42:34 +0200

    Python_APP

---
+++ Python_APP/employers_menu.py
@@ -0,0 +1,68 @@
+from Employees import Employee
+import Basic
+
+
+
+def menu():
+    print('\tEmployee management')
+    print('\t================')
+    print('\ta) View all employers')
+    print('\tb) Create a new employee')
+    print('\tc) Delete an employee')
+    print('\td) Main menu')
+
+    option = ''
+
+    while option != 'd':
+        option = input('Enter an option: ').lower()
+        if option == 'a':
+            Display_Employees()
+        if option == 'b':
+            Add_Employ()
+        if option == 'c':
+            Remove_Employ()
+
+
+
+
+def Add_Employ():
+    #eginda
+    ans = 1
+    while ans == 1:
+
+        e1 = Employee("", "", "", "", "", "","")
+
+        e1.setId()
+        e1.setName()
+        e1.setSurname()
+        e1.setPhone()
+        e1.setAddress()
+        e1.setEmail()
+        e1.setPassword()
+
+        Basic.BasicMethods.save_object(e1, 'employee_data.txt')
+        print("Employee Added Successfully ")
+        ans = int(input("Do you want to add a new employee? (1/0)"))
+        del e1
+
+
+# Function to Remove Employee with given Id
+def Remove_Employ():
+    #irakurri listan gorde listatik elegidu eta borratu idatzi berriz artxiboan
+    ans = 1
+    while ans == 1:
+        Basic.BasicMethods.delete_object(Employee,'employee_data.txt')
+
+        print("Employee Delete Successfully ")
+        ans = int(input("Do you want to delete another employee? (1/0)"))
+
+
+# Function to Display All Employees
+def Display_Employees():
+    Basic.BasicMethods.read_object(Employee, 'employee_data.txt')
+
+def main_menu():
+    from menu import menuGeneral
+    menuGeneral()
+
+
---
+++ Python_APP/users_menu.py
@@ -0,0 +1,65 @@
+from Users import User
+import Basic
+
+
+
+def menu():
+    print('\tUsers management')
+    print('\t================')
+    print('\ta) View all users')
+    print('\tb) Create a new user')
+    print('\tc) Delete an user')
+    print('\td) Main menu')
+
+    option = ''
+
+    while option != 'd':
+        option = input('Enter an option: ').lower()
+        if option == 'a':
+            Display_Users()
+        if option == 'b':
+            Add_User()
+        if option == 'c':
+            Remove_User()
+
+
+
+
+
+def Add_User():
+    #eginda
+    ans = 1
+    while ans == 1:
+
+        u1 = User ("", "", "", "", "", "")
+
+        u1.setId()
+        u1.setName()
+        u1.setSurname()
+        u1.setPhone()
+        u1.setEmail()
+        u1.setPassword()
+
+        Basic.BasicMethods.save_object(u1, 'user_data.txt')
+        print("User Added Successfully ")
+        ans = int(input("Do you want to add a new user? (1/0)"))
+        del u1
+
+
+def Remove_User():
+    #irakurri listan gorde listatik elegidu eta borratu idatzi berriz artxiboan
+    ans = 1
+    while ans == 1:
+        Basic.BasicMethods.delete_object(User,'user_data.txt')
+
+        print("User Delete Successfully ")
+        ans = int(input("Do you want to delete another user? (1/0)"))
+
+
+def Display_Users():
+    Basic.BasicMethods.read_user(User, 'user_data.txt')
+
+def main_menu():
+    from menu import menuGeneral
+    menuGeneral()
+
